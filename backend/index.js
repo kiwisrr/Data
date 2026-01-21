f26c9f05bea33af8a8ff729a0459b9772c893bef
async <asyncreact@gmail.com>
2026-01-21 15:00:34 -0400

    Data

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const publicPath = path.join(__dirname, "../frontend/public");
+const publicPath = path.join(__dirname, "public");
 
 console.log("Sirviendo archivos estáticos desde:", publicPath);
 
